abe962d4fc0eef1161fcc052f1838e981c9a3640
maiush <sm2783@cam.ac.uk>
2025-05-27 23:27:51 +0000

    update package

--- sft/all-lora.sh
+++ sft/all-lora.sh
@@ -1,38 +1,38 @@
-# ./lora.sh gemma-2-2b-it common_sense || true
-# ./lora.sh gemma-2-9b-it common_sense || true
-# ./lora.sh gemma-2-27b-it common_sense || true
+./lora.sh gemma-2-2b-it common_sense || true
+./lora.sh gemma-2-9b-it common_sense || true
+./lora.sh gemma-2-27b-it common_sense || true
 
-# ./lora.sh qwen-2.5-0.5b-it common_sense || true
-# ./lora.sh qwen-2.5-1.5b-it common_sense || true
-# ./lora.sh qwen-2.5-3b-it common_sense || true
-# ./lora.sh qwen-2.5-7b-it common_sense || true
-# ./lora.sh qwen-2.5-14b-it common_sense || true
-# ./lora.sh qwen-2.5-32b-it common_sense || true
-# ./lora.sh qwen-2.5-72b-it common_sense || true
+./lora.sh qwen-2.5-0.5b-it common_sense || true
+./lora.sh qwen-2.5-1.5b-it common_sense || true
+./lora.sh qwen-2.5-3b-it common_sense || true
+./lora.sh qwen-2.5-7b-it common_sense || true
+./lora.sh qwen-2.5-14b-it common_sense || true
+./lora.sh qwen-2.5-32b-it common_sense || true
+./lora.sh qwen-2.5-72b-it common_sense || true
 
-# ./lora.sh llama-3.1-8b-it common_sense || true
-# ./lora.sh llama-3.1-70b-it common_sense || true
+./lora.sh llama-3.1-8b-it common_sense || true
+./lora.sh llama-3.1-70b-it common_sense || true
 
-# ./lora.sh mistral-nemo-12b-it common_sense || true
-# ./lora.sh mistral-small-22b-it common_sense || true
-# ./lora.sh mistral-large-123b-it common_sense || true
+./lora.sh mistral-nemo-12b-it common_sense || true
+./lora.sh mistral-small-22b-it common_sense || true
+./lora.sh mistral-large-123b-it common_sense || true
 
 
-# ./lora.sh gemma-2-2b-it text_quality || true
-# ./lora.sh gemma-2-9b-it text_quality || true
-# ./lora.sh gemma-2-27b-it text_quality || true
+./lora.sh gemma-2-2b-it text_quality || true
+./lora.sh gemma-2-9b-it text_quality || true
+./lora.sh gemma-2-27b-it text_quality || true
 
-# ./lora.sh qwen-2.5-0.5b-it text_quality || true
-# ./lora.sh qwen-2.5-1.5b-it text_quality || true
-# ./lora.sh qwen-2.5-3b-it text_quality || true
-# ./lora.sh qwen-2.5-7b-it text_quality || true
-# ./lora.sh qwen-2.5-14b-it text_quality || true
+./lora.sh qwen-2.5-0.5b-it text_quality || true
+./lora.sh qwen-2.5-1.5b-it text_quality || true
+./lora.sh qwen-2.5-3b-it text_quality || true
+./lora.sh qwen-2.5-7b-it text_quality || true
+./lora.sh qwen-2.5-14b-it text_quality || true
 ./lora.sh qwen-2.5-32b-it text_quality || true
 ./lora.sh qwen-2.5-72b-it text_quality || true
 
-# ./lora.sh llama-3.1-8b-it text_quality || true
+./lora.sh llama-3.1-8b-it text_quality || true
 ./lora.sh llama-3.1-70b-it text_quality || true
 
-# ./lora.sh mistral-nemo-12b-it text_quality || true
+./lora.sh mistral-nemo-12b-it text_quality || true
 ./lora.sh mistral-small-22b-it text_quality || true
-# ./lora.sh mistral-large-123b-it text_quality || true
+./lora.sh mistral-large-123b-it text_quality || true
